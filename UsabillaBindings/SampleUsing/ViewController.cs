3ccd7a95560b51d732730eafc258993e375cd28e
Yaroslav Pelyukh <y.pelyukh@gmail.com>
2018-11-20 09:05:02 +0200

    Initial commit: added Bindings project; added simple demo project

---
+++ UsabillaBindings/SampleUsing/ViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using UsabillaNative;
+
+namespace SampleUsing
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+            Usabilla.LoadFeedbackForm("5a16d9c67d66810f2248aad9", null, IntPtr.Zero);
+        }
+	}
+}
+
---
+++ UsabillaBindings/XamarinBindingLibrary/ApiDefinition.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+
+namespace UsabillaNative
+{
+	[BaseType(typeof(NSObject), Name = "Usabilla")]
+	interface Usabilla
+	{
+        [Static, Export("initializeWithAppID:completion:")]
+        void Initialize([NullAllowed]string appId, [NullAllowed]Action completion);
+
+        [Static, Export("customVariables")]
+        NSDictionary<NSString, NSObject> CustomVariables { get; set; }
+
+        [Static, Export("localizedStringFile")]
+        String LocalizedStringFile { get; set; }
+
+        [Static, Export("theme")]
+        UsabillaTheme Theme { get; set; }
+
+        [Static, Export("dismissAutomatically")]
+        bool DismissAutomatically { get; set; }
+
+        [Static, Export("canDisplayCampaigns")]
+        bool CanDisplayCampaigns { get; set; }
+
+        [Static, Export("debugEnabled")]
+        bool debugEnabled { get; set; }
+
+        [Static, Export("sendEvent:")]
+        void SendEvent(string eventName);
+
+        [Static, Export("removeCachedForms")]
+        void RemoveCachedForms();
+
+        [Static, Export("resetCampaignData:")]
+        void ResetCampaignData(Action completion);
+
+        [Static, Export("preloadFeedbackForms:")]
+        void PreloadFeedbackForms(String[] formIDs);
+
+        [Static, Export("loadFeedbackForm:screenshot:theme:")]
+        void LoadFeedbackForm(string formID, [NullAllowed]UIImage image, IntPtr theme);
+
+        [Static, Export("takeScreenshot:")]
+        [NullAllowed] UIImage TakeScreenshot(UIView view);
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    [Protocol, Model]
+    interface UsabillaDelegate
+    {
+        [Export("formDidLoad:")]
+        void FormDidLoad(UINavigationController form);
+
+        [Export("formDidFailLoading:")]
+        void FormDidFailLoading(UBError form);
+
+        [Export("formDidClose:results:isRedirectToAppStoreEnabled")]
+        void FormDidClose(String formID, NSArray feedbackResults, bool isRedirectToAppStoreEnabled);
+
+        //func formWillClose(form: UINavigationController, formID: String, withFeedbackResults results: [FeedbackResult], isRedirectToAppStoreEnabled: Bool)
+        [Export("formWillClose:formID:results:isRedirectToAppStoreEnabled:")]
+        void FormWillClose(UINavigationController form, String formID, NSArray feedbackResults, bool isRedirectToAppStoreEnabled);
+
+        //func campaignDidClose(withFeedbackResult result: FeedbackResult, isRedirectToAppStoreEnabled: Bool)
+        [Export("campaignDidClose:isRedirectToAppStoreEnabled:")]
+        void CampaignDidClose(FeedbackResult feedbackResult, bool isRedirectToAppStoreEnabled);
+    }
+}
