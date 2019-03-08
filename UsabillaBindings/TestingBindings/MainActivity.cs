68c9ac07e5511dda8f2253be10683b4092afb3cf
Yaroslav Pelyukh <y.pelyukh@gmail.com>
2019-03-08 16:30:33 +0200

    Updated name of parameter in Usabilla binding project

--- UsabillaBindings/TestingBindings/MainActivity.cs
+++ UsabillaBindings/TestingBindings/MainActivity.cs
@@ -5,13 +5,28 @@ using System;
 using Android.Support.V7.App;
 using Usabilla;
 using Com.Usabilla.Sdk.Ubform.Sdk.Form;
-using Com.Usabilla.Sdk.Ubform.DI;
+
 
 namespace TestingBindings
 {
     [Activity(Label = "TestingBindings", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : AppCompatActivity, IUsabillaFormCallback
     {
+        public void FormLoadFail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FormLoadSuccess(IFormClient parameter)
+        {
+            parameter.Fragment.Show(SupportFragmentManager, "FORM");
+        }
+
+        public void MainButtonTextUpdated(string p0)
+        {
+
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -20,21 +35,8 @@ namespace TestingBindings
             Usabilla.Usabilla.Instance.Initialize(BaseContext, "71f49b32-c65d-4565-b923-3b176d053122");
             Usabilla.Usabilla.Instance.LoadFeedbackForm("59787ce6022bf728fc184e4f", this);
 
-        }
-
-        void IUsabillaFormCallback.FormLoadFail()
-        {
 
         }
 
-        void IUsabillaFormCallback.FormLoadSuccess(IFormClient p0)
-        {
-            p0.Fragment.Show(SupportFragmentManager, "FORM");
-        }
-
-        void IUsabillaFormCallback.MainButtonTextUpdated(string p0)
-        {
-
-        }
     }
 }
