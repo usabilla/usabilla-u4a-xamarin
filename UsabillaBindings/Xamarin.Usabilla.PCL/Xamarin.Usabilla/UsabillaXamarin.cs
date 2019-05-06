13f10d7addac0fe8a296101ca79b086d072ad1ee
gumil <miguel.panelo@usabilla.com>
2019-05-06 15:46:35 +0200

    Add common implementation of Usabilla interface

---
+++ UsabillaBindings/Xamarin.Usabilla.PCL/Xamarin.Usabilla/UsabillaXamarin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Usabilla.PCL;
+
+namespace Xamarin.Usabilla
+{
+    public class UsabillaXamarin : IUsabillaXamarin
+    {
+        public static UsabillaXamarin Instance { get; } = new UsabillaXamarin();
+
+        static UsabillaXamarin() { }
+        private UsabillaXamarin() { }
+
+        public void initialize(string appId, Func<object> callback)
+        {
+            throw new NotImplementedException("Dummy Implementation for Bait And Switch Trick and should not be called");
+        }
+    }
+}
