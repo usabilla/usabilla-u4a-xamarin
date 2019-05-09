4ef80dd2933ff30c42bea848680638149370726b
gumil <miguel.panelo@usabilla.com>
2019-05-09 10:22:47 +0200

    Add iOS implementation of Usabilla interface

---
+++ UsabillaBindings/Xamarin.Usabilla.iOS/UsabillaXamarin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Usabilla.PCL;
+
+namespace Xamarin.Usabilla
+{
+    public sealed class UsabillaXamarin : IUsabillaXamarin
+    {
+        public static UsabillaXamarin Instance { get; } = new UsabillaXamarin();
+
+        static UsabillaXamarin() { }
+        private UsabillaXamarin() { }
+
+        public void initialize(string appId, Func<object> callback)
+        {
+            Console.WriteLine("Initializing SDK for iOS");
+        }
+    }
+}
