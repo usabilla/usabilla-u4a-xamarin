17e491d22b70e1f60436c48d1192d6cf3ea66f83
gumil <miguel.panelo@usabilla.com>
2019-05-14 14:55:23 +0200

    Implement interface on Android

--- UsabillaBindings/Xamarin.Usabilla.Android/Xamarin.Usabilla.Android/UsabillaXamarin.cs
+++ UsabillaBindings/Xamarin.Usabilla.Android/Xamarin.Usabilla.Android/UsabillaXamarin.cs
@@ -1,19 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using Android.App;
 using Android.Util;
 using Xamarin.Usabilla.PCL;
 
 namespace Xamarin.Usabilla
 {
-    public sealed class UsabillaXamarin : IUsabillaXamarin
+    public class UsabillaXamarin : IUsabillaXamarin
     {
         public static UsabillaXamarin Instance { get; } = new UsabillaXamarin();
+        public bool DebugEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IDictionary<string, string> CustomVariables { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IList<string> DefaultMasks => throw new NotImplementedException();
 
         static UsabillaXamarin() { }
         private UsabillaXamarin() { }
 
-        public void initialize(string appId, Func<object> callback)
+        public void Initialize(string appId)
         {
             Log.Debug("UBInfo", "Initializing SDK");
+            UsabillaAndroid.Usabilla.Instance.Initialize(Application.Context, appId);
+        }
+
+        public void SendEvent(string anEvent)
+        {
+            Log.Debug("UBInfo", anEvent + " is sent");
+            UsabillaAndroid.Usabilla.Instance.SendEvent(Application.Context, anEvent);
+        }
+
+        public void Reset()
+        {
+            Log.Debug("UBInfo", "reset");
+            UsabillaAndroid.Usabilla.Instance.ResetCampaignData(Application.Context);
+        }
+
+        public void ShowFeedbackForm(string formId, Action<XUFormLoadResult> result)
+        {
+            Log.Debug("UBInfo", "show " + formId);
+        }
+
+        public void ShowFeedbackFormWithScreenshot(string formId, Action<XUFormLoadResult> result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Dismiss()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDataMasking(IList<string> masks, char maskCharacter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
