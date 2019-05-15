ce0f5a7916fc002976c588b077f83d652696c4ef
Anders Liebl <anders.liebl@usabilla.com>
2019-05-15 10:44:26 +0200

    Add SendEvent and Reset method Add textfield and buttons to sample app

--- UsabillaBindings/Xamarin.Usabilla.PCL/IUsabillaXamarin.cs
+++ UsabillaBindings/Xamarin.Usabilla.PCL/IUsabillaXamarin.cs
@@ -4,5 +4,9 @@ namespace Xamarin.Usabilla.PCL
     public interface IUsabillaXamarin
     {
         void Initialize(String appId);
+
+        void SendEvent(string anEvent);
+
+        void Reset();
     }
 }
--- UsabillaBindings/Xamarin.Usabilla.PCL/Xamarin.Usabilla/UsabillaXamarin.cs
+++ UsabillaBindings/Xamarin.Usabilla.PCL/Xamarin.Usabilla/UsabillaXamarin.cs
@@ -12,7 +12,15 @@ namespace Xamarin.Usabilla
 
         public void Initialize(string appId)
         {
-            throw new NotImplementedException("Dummy Implementation for Bait And Switch Trick and should not be called");
+            throw new NotImplementedException("Dummy Implementation for Initialize and should not be called");
+        }
+        public void SendEvent(string anEvent)
+        {
+            throw new NotImplementedException("Dummy Implementation for SendEvent and should not be called");
+        }
+        public void Reset()
+        {
+            throw new NotImplementedException("Dummy Implementation for Reset and should not be called");
         }
     }
 }
